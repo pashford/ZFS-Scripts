0d45492d050b392bcf2155fb016fae778d0c4859
Peter Ashford <ashford@accs.com>
2014-12-14 10:57:45 -0800

    Move retention time to a variable

--- Linux/zfs.log.sh
+++ Linux/zfs.log.sh
@@ -2,8 +2,9 @@
 
 BASEDIR=~/zfs.log
 DTG=`date +%Y%m%d.%H%M%S`
+RETENTION=90
 
 /bin/cat /proc/spl/kstat/zfs/arcstats > ${CURFILE}
 /bin/cat /proc/spl/kmem/slab > ${BASEDIR}/slab.${DTG}
 /bin/cat /proc/slabinfo > ${BASEDIR}/slabinfo.${DTG}
-/bin/find ${BASEDIR}/log.* ${BASEDIR}/slab.* ${BASEDIR}/slabinfo.* -mtime +90 -delete
+/bin/find ${BASEDIR}/log.* ${BASEDIR}/slab.* ${BASEDIR}/slabinfo.* -mtime +${RETENTION} -delete
